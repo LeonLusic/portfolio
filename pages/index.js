59b8411a6cb09b54bf18112f5f7b2d9b063e5f15
Leon Lusic <l.lusic@student.vu.nl>
2021-12-19 00:56:33 +0100

    Initial changes to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,54 @@
 import Head from 'next/head'
 
+var loremipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius orci non lacinia finibus. Aenean id erat aliquet odio euismod varius in at massa. Etiam et purus in urna efficitur gravida et eu orci. Proin nec metus id ante consequat laoreet. Etiam egestas, nibh id feugiat congue, magna diam tristique erat, nec tempus diam neque at erat. In facilisis, diam sit amet ultrices finibus, diam purus hendrerit tortor, ut aliquam nibh augue vel nulla. Suspendisse lobortis feugiat sem eget pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam et quam vitae leo malesuada suscipit. Quisque vel massa dictum elit pretium condimentum. Curabitur volutpat, justo eget viverra tincidunt, mauris sem aliquam tortor, sed vulputate est tellus rutrum erat.'
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-greengradient">
       <Head>
-        <title>Create Next App</title>
+        <title>Leon Lusic Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <header>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      </header>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <body className='min-h-full h-screen py-10'>
+        <div className="flex justify-center text-6xl text-white w-full max-w-screen-xl shadow-lg p-6 bg-greengradient">
+          <h1>
+            Hello, I'm Leon. I am a mathematician and NFT developer.
+          </h1>
         </div>
-      </main>
+        <div className='justify-center py-4 text-2xl text-white'>
+          <p className='py-2'>
+            As a very talented mathematician, I turned towards programming and NFT development.
+          </p>
+          <p className='py-2'>
+            I am currently finishing my third year of my math bachelor degree at VU Amsterdam.
+          </p>
+        </div>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <footer className="flex items-center w-full h-fit max-h-24 border-t text-2xl uppercase text-white object-bottom fixed">
+          <span className='align-center'>Social media links:</span>
+          <a
+            className="flex items-center justify-center"
+            href="https://github.com/LeonLusic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/github.svg" alt="Github Logo" className="h-6 ml-2 px-1" />
+          </a>
+          <a
+            className="flex items-center justify-center"
+            href="https://www.instagram.com/leonlusic/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/instagram.svg" alt="Instagram Logo" className="h-7 ml-2 px-1" />
+          </a>
+        </footer>
+      </body>
     </div>
   )
 }
